7e8d5402413f56845a55718ed0f57ea29f55bdc8
omrlns <marlon.da.silva996@gmail.com>
2025-08-13 08:05:54 -0300

    reformulando o EX001

--- Aula 03/EX001.py
+++ Aula 03/EX001.py
@@ -1,34 +1,14 @@
-# você acabou de se formar e conseguiu um emprego. 
-# você quer comprar uma casa. Mas as casas são caras e você precisa economizar o que sobra de seu salário.
-# seu objetivo é descobrir quantos meses é preciso economizar para conseguir pagar o preço de entrada da casa.
-# seu programa deve perguntar ao usuário as seguintes informações e armazenar elas como floats:
-# a) seu salário mensal
-# b) a porcentagem do salário que você consegue economizar. (Exemplo: 12,5%)
-# c) o preço da casa
-# assuma que você consegue comprar a casa pagando um valor de entrada de 25% do preço da casa,
-# e que no começo da cada mês, o valor que você economizou aumenta em 0,5% (pois está em uma caderneta de poupança).
-# após receber os valores de entrada, o programa deve gerar uma resposta em meses.
+# qual o valor das variáveis 's1' e 's2' após executar o código abaixo?
 
-salario = float(input('informe o seu salário mensal: R$'))
-percentual_economia = float(input('digite a porcentagem que consegue economizar (%): '))
-preco_casa = float(input('digite o preço da casa: R$ '))
+b = ":"
+c = ")"
+s1 = b + 2*c
 
-# cálculo do valor de entrada (25% do preço da casa)
-entrada_casa = preco_casa * 0.25
+print(s1)
 
-# conversão da porcentagem para valor mensal
-economia_mensal = salario * (percentual_economia / 100)
+f = "a"
+g = " b"
+h = "3"
+s2 = (f+g) * int(h)
 
-# juros mensais (0,5%)
-taxa_juros = 0.005
-
-# contadores
-saldo = 0
-meses = 0
-
-while (saldo < entrada_casa):
-    saldo += economia_mensal # economia do mês
-    saldo += saldo * taxa_juros # rendimento da poupança
-    meses += 1
-
-print(f'você precisará de {meses} meses para juntar o valor da entrada.')
+print(s2)
