7318a09d3a3be5b46beca8f5d3c6d96929e8326f
omrlns <marlon.da.silva996@gmail.com>
2025-08-13 08:20:32 -0300

    resolução do EX004

---
+++ Aula 03/EX004.py
@@ -0,0 +1,9 @@
+# solicita os valores de entrada
+c = float(input('digite o valor de c: '))
+g = float(input('digite o chute inicial g: '))
+
+# calcula novoG usando a fórmula dada
+novoG = g - (g**3 - c) / (3 * g**2)
+
+# mostra o resultado
+print(f"O novo chute mais próximo do zero é: {novoG}")
