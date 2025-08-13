0f7195b7229ca01181bfac5dc0e393cbbc03c7aa
omrlns <marlon.da.silva996@gmail.com>
2025-08-12 23:13:31 -0300

    resolução de um exercício rápido

---
+++ Aula 03/revisão.py
@@ -0,0 +1,8 @@
+# a) peça um verbo para o usuário
+verbo = input('digite um verbo: ')
+
+# b) imprima a frase substituindo o espaço pelo verbo
+print(f'eu posso {verbo} melhor do que você!')
+
+# c) e o verbo 5 vezes separado por espaço na linha seguinte
+print((verbo + ' ') * 5)
