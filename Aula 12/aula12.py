2473bf49843017e412b644ec1f74e0fc330ff1c3
omrlns <marlon.da.silva996@gmail.com>
2025-10-01 08:37:04 -0300

    feat: add teste com tipos de erros

---
+++ Aula 12/aula12.py
@@ -0,0 +1,18 @@
+# Exceções e Asserções
+
+# numeros = [1, 2, 3]
+# IndexError: list index out of range
+# print(numeros[4])
+
+# TypeError: int() argument must be a string, a bytes-like object or a real number, not 'type'
+# inteiro = int(list)
+
+# NameError: name 'vogais' is not defined
+# print(vogais)
+
+# TypeError: unsupported operand type(s) for /: 'str' and 'int'
+# print('4' / 2)
+    
+
+
+
