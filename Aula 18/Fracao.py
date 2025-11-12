789984daca6b8ff5f9af2c23aea271eab43b1a96
imarlonds <marlon.silva@aluno.fmpsc.edu.br>
2025-11-12 10:28:46 -0300

    class Fracao com __str__ , __add__ e __mult__

--- Aula 18/Fracao.py
+++ Aula 18/Fracao.py
@@ -4,7 +4,22 @@ class Fracao(object):
         self.denominador = denominador
 
     def __str__(self):
-        return str(self.numerador) + '/' + str(self.denominador)
+        if self.denominador == 1:
+            return str(self.numerador)
+        else:
+             return str(self.numerador) + '/' + str(self.denominador)
+    
+    def __add__(self, outraFracao):
+        # deve retornar uma fração
+        parteCima = self.numerador * outraFracao.denominador + outraFracao.numerador * self.denominador
+        parteBaixo = self.denominador * outraFracao.denominador
+        return Fracao(parteCima, parteBaixo)
+
+    def __mul__(self, outraFracao):
+        # deve retornar uma fração
+        parteCima = self.numerador * outraFracao.numerador
+        parteBaixo = self.denominador * outraFracao.denominador
+        return Fracao(parteCima, parteBaixo)
         
     def obterInverso(self):
         # retorna o inverso da fração atual
@@ -14,7 +29,18 @@ class Fracao(object):
         # transforma a fração atual para que ela fique com o valor inverso.
         self.numerador, self.denominador = self.denominador, self.numerador
 
-fracaoA = Fracao(3, 4)
-print(fracaoA.obterInverso())
-fracaoA.inverte()
-print(fracaoA.numerador, fracaoA.denominador)
+# fracaoA = Fracao(3, 4)
+# print(fracaoA.obterInverso())
+# fracaoA.inverte()
+# print(fracaoA.numerador, fracaoA.denominador)
+
+# fracaoB = Fracao(2, 3)
+# mult = fracaoA * fracaoB
+# print(mult)
+
+fracaoC = Fracao(3, 1)
+# print(fracaoC)
+
+fracaoD = Fracao(2, 2)
+add = fracaoC + fracaoD
+print(add)
