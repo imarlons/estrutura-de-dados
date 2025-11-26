402c3060577c9e1795625571010f21480db85025
imarlonds <marlon.silva@aluno.fmpsc.edu.br>
2025-11-25 21:44:30 -0300

    resolução do EX007

---
+++ Exercícios/Revisão para Prova/EX007.py
@@ -0,0 +1,13 @@
+# implemente uma função intercala(l1, l2) que recebe duas listas l1 e l2 de mesmo tamanho 
+# e retorna uma nova lista contendo os elementos intercalados.
+# exemplo: intercala([1,2,3], [’a’,’b’,’c’]) 
+# deve produzir [1, ’a’, 2, ’b’, 3, ’c’]
+
+def intercala(listaA, listaB):
+    resultado = []
+    for i in range(len(listaA)):
+        resultado.append(listaA[i])
+        resultado.append(listaB[i])
+    return resultado
+
+print(intercala([1, 2, 3], ['a', 'b', 'c']))
